3aca5c70c21cdfcce3d51f4a14c316f0b0050ef9
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-06 21:18:07 +0100

    fine del secondo snack

---
+++ jsnack2/js/script.js
@@ -0,0 +1,42 @@
+// dobbiamo chiedere all'utente una parola
+const buttonElement = document.querySelector("#avvia");
+
+let pallina;
+
+buttonElement.addEventListener("click",
+function() {
+
+    function trovaPalindromo(parola) {
+
+        for (let i=0; i < Math.floor(parola.length / 2); i++) {
+
+            if (parola[i] !== parola[parola.length - 1 - i]) {
+
+
+                return false;
+            }
+        
+        }
+
+        return true;
+
+    }
+
+    pallina = prompt("inserisci una parola ");
+
+    let text = trovaPalindromo(pallina);
+    
+if (text == false) {
+
+    document.getElementById("parola").innerHTML = ('questa non è una parola palindroma');
+
+} else {
+
+    document.getElementById("parola").innerHTML = ('questa è una parola palindroma')
+    
+}
+
+})
+// questa parola deve essere controllata se è palindroma o meno
+// mettere delle condizioni dalla quale possiamo ricavare la parola se è palindroma o no
+// stamparte la parola in pagina se è palindroma con un messaggio che dice che pa parola selezionata è palindroma idem se non lo è.
